52717b58407c06e2db5e12e95f18f94ab009adfd
BrianMenezesUT <47682152+BrianMenezesUT@users.noreply.github.com>
2019-10-27 12:30:02 -0500

    Revert "yeet"
    This reverts commit abd92d6cb4221309bd6e02443c929dcbfce5e2b7.

--- scripts/quickstartsim.sh
+++ scripts/quickstartsim.sh
@@ -4,26 +4,10 @@ roslaunch mission8_sim droneOnly.launch &
 
 sleep 5
 
-mkfifo /tmp/status
-function setGuided
-{
-	grep -m 4 GPS </tmp/status
-	echo 'mode GUIDED'
-}
-cd ~/ardupilot/ArduCopter/ && setGuided | sim_vehicle.py -f gazebo-iris -I0 >/tmp/status &
+cd ~/ardupilot/ArduCopter/ && echo 'mode GUIDED' | sim_vehicle.py -f gazebo-iris --console -I0  &
 
 roslaunch out_of_controls apm.launch &
 
-rosrun out_of_controls tracking &
+rosrun out_of_controls tracking
 
-echo 'waiting for GPS connection'
-
-while true; do;
-	echo '[q]uit [k]ill'
-	in= read
-	[[in=='q']] && break
-	if [[in=='k']]; then
-		pkill -s 0 
-		break
-	fi
-done
+wait
